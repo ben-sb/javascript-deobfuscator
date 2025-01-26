25bc316ff91cc4b41f2bf0fc70d06acef7cdc553
ben-sb <bensb1@protonmail.com>
2025-01-26 20:29:25 +0000

    feat: support JS Obfuscator XOR string encoding

--- src/modifications/expressions/stringDecoder.ts
+++ src/modifications/expressions/stringDecoder.ts
@@ -11,9 +11,21 @@ import TraversalHelper from '../../helpers/traversalHelper';
  *
  * Strings split into char codes:
  * String.fromCharCode(72,101,108,108,111,32,87,111,114,108,100) -> "Hello World"
+ *
+ * JS-Obfuscator specific XOR string encoding:
+ * function _0x6f26a(_4,_5){_5=9;var _,_2,_3="";_2=_4.split(".");for(_=0;_<_2.length-1;_++){_3+=String.fromCharCode(_2[_]^_5);}return _3;}console['\x6c\x6f\x67'](_0x6f26a("65.108.101.101.102.41.94.102.123.101.109."));
+ * ->
+ * console["log"]("Hello World")
  */
 
+interface StringEncodingFunctionProps {
+    name: string;
+    key: number;
+}
+
 export default class StringDecoder extends Modification {
+    private encodingFunctionProps?: StringEncodingFunctionProps;
+
     /**
      * Creates a new modification.
      * @param ast The AST.
@@ -26,7 +38,27 @@ export default class StringDecoder extends Modification {
      * Executes the modification.
      */
     execute(): void {
+        this.findStringEncodingFunction();
         this.undoStringOperations();
+    }
+
+    /**
+     * Looks for a JS-Obfuscator XOR string encoding function.
+     */
+    private findStringEncodingFunction() {
+        const self = this;
+
+        // only look at top level
+        // TODO: once scope system has been improved, rework this
+        for (const statement of this.ast.statements) {
+            if (self.isXorStringEncodingFunction(statement)) {
+                const name = statement.name.name;
+                const key = statement.body.statements[0].expression.expression.value;
+                this.encodingFunctionProps = { name, key };
+
+                TraversalHelper.removeNode(this.ast, statement);
+            }
+        }
     }
 
     /**
@@ -63,6 +95,23 @@ export default class StringDecoder extends Modification {
                         value: String.fromCharCode.apply(undefined, charCodes)
                     });
                     TraversalHelper.replaceNode(parent, node, str);
+                } else if (
+                    self.encodingFunctionProps != undefined &&
+                    self.isStringEncodingFunctionCall(node)
+                ) {
+                    /**
+                     * Handles strings obfuscated with JS-Obfuscator XOR string encoding.
+                     */
+                    const arg = node.arguments[0].value;
+                    const decodedStr = arg
+                        .split('.')
+                        .slice(0, -1)
+                        .map(c =>
+                            String.fromCharCode(parseInt(c) ^ self.encodingFunctionProps!.key)
+                        )
+                        .join('');
+                    const replacement = new Shift.LiteralStringExpression({ value: decodedStr });
+                    TraversalHelper.replaceNode(parent, node, replacement);
                 }
             }
         });
@@ -111,6 +160,51 @@ export default class StringDecoder extends Modification {
             node.callee.object.name === 'String'
         );
     }
+
+    /**
+     * Returns whether a node is the JS-Obfuscator XOR string encoding function.
+     * @param node The AST node.
+     * @returns Whether.
+     */
+    private isXorStringEncodingFunction(node: Shift.Node): node is StringEncodingFunction {
+        return (
+            node.type === 'FunctionDeclaration' &&
+            node.params.items.length === 2 &&
+            node.params.rest == undefined &&
+            node.body.statements.length === 5 &&
+            node.body.statements[0].type === 'ExpressionStatement' &&
+            node.body.statements[0].expression.type === 'AssignmentExpression' &&
+            node.body.statements[0].expression.binding.type === 'AssignmentTargetIdentifier' &&
+            node.body.statements[0].expression.expression.type === 'LiteralNumericExpression' &&
+            node.body.statements[1].type === 'VariableDeclarationStatement' &&
+            node.body.statements[1].declaration.declarators.length === 3 &&
+            node.body.statements[2].type === 'ExpressionStatement' &&
+            node.body.statements[3].type === 'ForStatement' &&
+            node.body.statements[3].init?.type === 'AssignmentExpression' &&
+            node.body.statements[3].test?.type === 'BinaryExpression' &&
+            node.body.statements[3].update?.type === 'UpdateExpression' &&
+            node.body.statements[3].body.type === 'BlockStatement' &&
+            node.body.statements[4].type === 'ReturnStatement' &&
+            node.body.statements[4].expression?.type === 'IdentifierExpression'
+        );
+    }
+
+    /**
+     * Returns whether a node is a call of the string encoding function.
+     * @param node The AST node.
+     * @returns Whether.
+     */
+    private isStringEncodingFunctionCall(node: Shift.Node): node is StringEncodingFunctionCall {
+        return (
+            this.encodingFunctionProps != undefined &&
+            node.type === 'CallExpression' &&
+            node.callee.type === 'IdentifierExpression' &&
+            node.callee.name === this.encodingFunctionProps.name &&
+            node.arguments.length === 1 &&
+            node.arguments[0].type === 'LiteralStringExpression' &&
+            /^(\d{1,3}\.)+$/.test(node.arguments[0].value) // matches "65.108.101." etc
+        );
+    }
 }
 
 type ReversedStringExpression = Shift.CallExpression & {
@@ -128,3 +222,19 @@ type ReversedStringExpression = Shift.CallExpression & {
 };
 
 type CharCodesString = Shift.CallExpression & { arguments: Shift.LiteralNumericExpression[] };
+
+type StringEncodingFunction = Shift.FunctionDeclaration & {
+    body: Shift.FunctionBody & {
+        statements: [
+            Shift.ExpressionStatement & {
+                expression: Shift.AssignmentExpression & {
+                    expression: Shift.LiteralNumericExpression;
+                };
+            }
+        ];
+    };
+};
+
+type StringEncodingFunctionCall = Shift.CallExpression & {
+    arguments: [Shift.LiteralStringExpression];
+};
